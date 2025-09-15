e53e256931c83acab167542dd0a6516182c921e1
Favioman <carlos76190404@gmail.com>
2025-09-14 19:02:12 -0600

    Tabla Usuario

---
+++ src/main/java/pe/edu/upc/trabajogrupo2/repositories/IUsuarioRepository.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.trabajogrupo2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.trabajogrupo2.entities.Usuarios;
+
+@Repository
+public interface IUsuarioRepository extends JpaRepository<Usuarios, Integer> {
+}
