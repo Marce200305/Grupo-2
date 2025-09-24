83e72392276822c0df7b048c03c4f0539bf6f876
Favioman <carlos76190404@gmail.com>
2025-09-24 04:38:26 -0600

    añadiendo security

--- src/main/java/pe/edu/upc/trabajogrupo2/repositories/IUsuarioRepository.java
+++ src/main/java/pe/edu/upc/trabajogrupo2/repositories/IUsuarioRepository.java
@@ -1,9 +1,19 @@
 package pe.edu.upc.trabajogrupo2.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pe.edu.upc.trabajogrupo2.entities.Usuarios;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuarios, Integer> {
+    public Usuarios findOneByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into roles (rol, user_id) VALUES (:rol, :user_id)", nativeQuery = true)
+    public void insRol(@Param("rol") String authority, @Param("user_id") Integer user_id);
 }
